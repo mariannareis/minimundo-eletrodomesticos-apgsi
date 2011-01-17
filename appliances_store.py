71aab9f6e81db9b23e4252c6cc2a33c725518d4d
mariannareis <mari.ounaum@gmail.com>
2011-01-17 15:17:56 -0200

    Added the warranty verification and more

--- appliances_store.py
+++ appliances_store.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Customer():
     def __init__(self, my_id, name, address):
         self.id = my_id
@@ -7,12 +9,16 @@ class Customer():
 class Product():
 
     def __init__(self, my_id, mark, model, serial_number):
-        self.warranty = {
-                         'id':my_id,
-                         'mark':mark,
-                         'model':model,
-                         'serial_number':serial_number,
-                         }
+        self.id = my_id
+        self.mark = mark
+        self.model = model
+        self.serial_number = serial_number
+#        self.warranty = {
+#                         'id':my_id,
+#                         'mark':mark,
+#                         'model':model,
+#                         'serial_number':serial_number,
+#                         }
 
 class Purchase():
 
@@ -20,4 +26,16 @@ class Purchase():
         self.id = my_id
         self.customer = customer
         self.date = date
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def is_my_purchase_in_the_warranty(self, purchase_id):
+        warranty_year = date.today().year - 1
+        warranty_date = date(warranty_year, date.today().month, date.today().day)
+        if date(2010, 1, 17) <= warranty_date:
+            return True
+        else:
+            return False
 
--- test_appliances_store.py
+++ test_appliances_store.py
@@ -7,23 +7,46 @@ from appliances_store import Product, Customer, Purchase
 class TestProduct(unittest.TestCase):
 
     def test_add_a_product(self):
-        product = Product(1, "Arno", "MD-001", "SN-3122")
-        product.warranty['id']|should| equal_to (1)
-        product.warranty['mark'] |should| equal_to ("Arno")
-        product.warranty['model'] |should| equal_to ("MD-001")
-        product.warranty['serial_number'] |should| equal_to ("SN-3122")
+        self.product = Product(1, "Arno", "MD-001", "SN-3122")
+        self.product.id |should| equal_to (1)
+        self.product.mark |should| equal_to ("Arno")
+        self.product.model |should| equal_to ("MD-001")
+        self.product.serial_number |should| equal_to ("SN-3122")
 
 class TestCustomer(unittest.TestCase):
+
     def test_add_a_costumer(self):
-        customer = Customer(1, "Paula", "Casa Da Paula")
-        customer.id |should| equal_to (1)
-        customer.name |should| equal_to ("Paula")
-        customer.address |should| equal_to ("Casa Da Paula")
+        self.customer = Customer(1, "Paula", "Casa Da Paula")
+        self.customer.id |should| equal_to (1)
+        self.customer.name |should| equal_to ("Paula")
+        self.customer.address |should| equal_to ("Casa Da Paula")
 
 class TestPurchase(unittest.TestCase):
     def test_making_a_purchase(self):
-        purchase = Purchase(1, 1, "17/01/2011") #id, customer, date_of_purchase
-        purchase.id |should| equal_to (1)
-        purchase.customer |should| equal_to(1)
-        purchase.date |should| equal_to ("17/01/2011")
+        self.purchase = Purchase(1, 1, "17/01/2011") #id, customer, date_of_purchase
+        self.purchase.id |should| equal_to (1)
+        self.purchase.customer |should| equal_to(1)
+        self.purchase.date |should| equal_to ("17/01/2011")
+        self.purchase.add_product(1)
+        self.purchase.add_product(1)
+        self.purchase.products |should| equal_to([1, 1])
+
+    def test_a_purchase_in_the_warranty(self):
+        self.purchase = Purchase(1, 1, "17/01/2010") #id, customer, date_of_purchase
+        self.purchase.id |should| equal_to (1)
+        self.purchase.customer |should| equal_to(1)
+        self.purchase.date |should| equal_to ("17/01/2010")
+        self.purchase.add_product(1)
+        self.purchase.add_product(1)
+        self.purchase.products |should| equal_to([1, 1])
+        self.purchase.is_my_purchase_in_the_warranty(1) |should| equal_to(True)
+
+
+
+#class Exchange():
+#    pass
+
+
+#class ProductStorage():
+#    pass
 
