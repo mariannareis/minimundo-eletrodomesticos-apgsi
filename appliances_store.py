cae9ff2f8aa18d5d6520281d6ae9699626b08fe6
mariannareis <mari.ounaum@gmail.com>
2011-01-17 12:39:38 -0200

    created the basic structure of the Product with de Warranty

--- appliances_store.py
+++ appliances_store.py
@@ -2,5 +2,8 @@ class Product():
 
     def __init__(self):
         self.warranty = {'date_of_purchase':'17/01/2011',
-                         'mark':'Arno'}
+                         'mark':'Arno',
+                         'model':'H3-42X',
+                         'serial_number':'123456',
+                         }
 
--- test_product.py
+++ test_product.py
@@ -9,4 +9,8 @@ class TestProduct(unittest.TestCase):
         product = Product()
         product.warranty.has_key('date_of_purchase') |should| equal_to (True)
         product.warranty.has_key('mark') |should| equal_to (True)
+        product.warranty.has_key('model') |should| equal_to (True)
+        product.warranty.has_key('serial_number') |should| equal_to (True)
+
+ #git commit -m 'created the basic structure of the Product with de Warranty'
 
