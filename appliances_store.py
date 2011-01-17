f48c5e2943e93bdc5c64f3a2c242dfb1de04ab49
mariannareis <mari.ounaum@gmail.com>
2011-01-17 13:07:12 -0200

    Improved the way to enter with a Product on the system

--- appliances_store.py
+++ appliances_store.py
@@ -1,15 +1,18 @@
-class Product():
-
-    def __init__(self):
-        self.warranty = {'date_of_purchase':'17/01/2011',
-                         'mark':'Arno',
-                         'model':'H3-42X',
-                         'serial_number':'123456',
-                         }
-
 class Customer():
     def __init__(self, my_id, name, address):
         self.id = my_id
         self.name = name
         self.address = address
+
+class Product():
+
+    def __init__(self, my_id, date_of_purchase, mark, model, serial_number, customer):
+        self.warranty = {
+                         'id':my_id,
+                         'date_of_purchase':date_of_purchase,
+                         'mark':mark,
+                         'model':model,
+                         'serial_number':serial_number,
+                         'initial_customer':customer,
+                         }
 
